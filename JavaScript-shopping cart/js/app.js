5784e37e7a3c28b5e7eb49331a4cba6e6e1be3c2
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2021-09-21 00:08:54 +0300

    Update app.js

--- JavaScript-shopping cart/js/app.js
+++ JavaScript-shopping cart/js/app.js
@@ -16,7 +16,7 @@
   let imageCounter = 0;
   //use a forEach loop to get a copy of all the images and push into an array of items
   images.forEach(function(image){
-    //push each imageto the array of images
+    //push each image to the array of images
     imageList.push(image.src);
   })
   
@@ -64,15 +64,3 @@
   
   
 })();
-
-
-/*
-Notes during project
-1. Tried to add an event listener to a node list. Had to actually do a forEach on the node list.
-https://stackoverflow.com/questions/55667793/uncaught-typeerror-input-addeventlistener-is-not-a-function
-2. Tried to use style.classList and then .show instead of just the string show. 'style not needed'
-3. How to access the individual CSS properties (without simply adding a class)
-4. I struggled with change the background. I thought I would be able to simply use element.style.background = 'image' but I realized I needed to use 'url(img);
-5. Had a bug in my code where I misspelled background. backgound vs. background 
-
-*/
